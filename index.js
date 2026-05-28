4acb71aee27412fa0ab16f305f460e74e39ce9e8
wushan2025-8 <wushan2025_8@126.com>
2026-05-28 17:12:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import { env } from "alibaba:workers";
-
-const test1 = env.test;
-const test2 = env.test2;
-const test3 = env.test3;
-const test4 = env.test4;
-const test5 = env.test5;
-const tEst5 = env.tEst5;
-const Test5 = env.Test5;
 
 export default {
   async fetch(request, context) {
-    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, Test5 is : ${Test5}`);
+    const { env } = await import("alibaba:workers");
+    const test1 = env.test;
+    const test2 = env.test2;
+    const test3 = env.test3;
+    const test4 = env.test4;
+    const test5 = env.test5;
+    const tEst5 = env.tEst5;
+    const Test5 = env.Test5;
+    
+    return new Response(`test1: ${test1}, test2: ${test2}, test3: ${test3}, test4: ${test4} ,test5: ${test5}, tEst5: ${tEst5}, Test5: ${Test5}`);
   },
 };
