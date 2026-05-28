d43e15f14797940fa568cfd4b4dad4284f4b3ffe
wushan2025-8 <wushan2025_8@126.com>
2026-05-28 16:58:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ export default {
     const test4 = env.test4;
     const test5 = env.test5;
     const tEst5 = env.tEst5;
-    const tesT6 = env.tesT6;
+    const Test5 = env.Test5;
     
 
-    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, tesT6 is : ${tesT6}`);
+    return new Response(`test1 is : ${test1}, test2 is : ${test2}, test3 is : ${test3}, test4 is : ${test4} ,test5 is : ${test5}, tEst5 is : ${tEst5}, Test5 is : ${Test5}`);
   },
 };
